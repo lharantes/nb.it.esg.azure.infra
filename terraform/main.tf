ed88f37be60f8ad9af6fe1031c9cee80a12f2658
USERSAD\lhenrina <lhenrina@emeal.nttdata.com>
2023-12-29 16:06:24 +0000

    folders management

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,3 @@
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = false
-    }
-  }
-}
-
 data "azurerm_client_config" "current" {}
 
 # ---------------------------------------------------------------------------------------------------------------------
