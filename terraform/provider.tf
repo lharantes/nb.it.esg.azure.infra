0e379ba709916ab5b7a41ec1a67b4c5f437e7925
USERSAD\lhenrina <lhenrina@emeal.nttdata.com>
2023-12-29 16:22:29 +0000

    Remove key vault

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,11 @@
 provider "azurerm" {
-  features {}
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = false
+    }
+  }
+  subscription_id = "8d7c6fb5-f932-4e13-ba06-fdf554f7527a"
+  tenant_id       = "637be5c4-cfcf-4c1e-af85-fd7ecadb26a2"
 }
 
-# data "azurerm_client_config" "current" {}
+data "azurerm_client_config" "current" {}
